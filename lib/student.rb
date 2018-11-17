d0be1c7aafdf7ebf9a93b837308929a57a990a1e
Erin Green <erin.y.green@gmail.com>
2018-11-17 20:33:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,10 @@ class Student
 
   def self.new_from_db(row)
     sql = <<-SQL
-      
+      new_student = Student.new
+      new_student.id = row[0]
+      new_student.name = row[1]
+      new_student.grade = row[2]
   end
 
   def self.all
