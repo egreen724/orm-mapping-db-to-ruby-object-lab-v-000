d3eb5bbb1b44d6e503db571be41f182fdedcd4ec
Erin Green <erin.y.green@gmail.com>
2018-11-17 20:43:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,11 +42,11 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.all_students_in_grade_9
+  def self.students_below_12th_grade
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 9 
+      WHERE grade < 12 
     SQL
     
     DB[:conn].execute(sql)
