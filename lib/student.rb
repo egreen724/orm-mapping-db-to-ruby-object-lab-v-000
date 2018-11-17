1b303b9d7ca19b1a59530fb63ba0ca246c0e0fa1
Erin Green <erin.y.green@gmail.com>
2018-11-17 20:44:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
     DB[:conn].execute(sql)
   end
   
-   def self.students_below_12th_grade
+   def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT *
       FROM students
